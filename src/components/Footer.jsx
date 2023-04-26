d135d0d11670d47b52a1f5c5a5d2faeeae2ecf23
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-26 17:13:16 -0400

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,21 @@
+import { Github } from "react-bootstrap-icons";
+
 export default function Footer() {
 
+    const gitHubUrl = "https://github.com/megstamp/portfolio"
     const currentYear = new Date().getFullYear();
-    const gitHubLink = "https://github.com/megstamp/portfolio"
 
     return(
-        <>
-        <div><small>&copy;</small> { currentYear }
-        <br />
-        <a href={ gitHubLink } target="blank"
-        rel="noreferrer">Code in Github </a>
-
-        </div>
-        </>
+        <footer>
+        <p className="text-center">
+        <a href={ gitHubUrl } 
+        target="_blank"
+        rel="noreferrer"
+            className="button-effect">
+            <Github size="60" color="black"/>
+            </a>
+            <small>&copy;</small> { currentYear }
+        </p>
+        </footer>
     )
 }
